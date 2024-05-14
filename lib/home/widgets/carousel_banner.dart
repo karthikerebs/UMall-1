917dbe8ed1ddcc2437cc1b0ec5228fc6b320425a
karthik <karthik.udayakumar11@gmail.com>
2024-05-14 12:19:51 +0530

    carousel changes

--- lib/home/widgets/carousel_banner.dart
+++ lib/home/widgets/carousel_banner.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-// import 'package:umall_app/home/constants/size.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class CarouselBanner extends StatelessWidget {
   CarouselBanner({
@@ -8,23 +9,57 @@ class CarouselBanner extends StatelessWidget {
   });
 
   final bannerImages = [
-    Image.asset('assets/banner.png'),
-    Image.asset('assets/banner.png'),
-    Image.asset('assets/banner.png')
+    'assets/banner.png',
+    'assets/banner.png',
+    'assets/banner.png'
   ];
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      items: bannerImages,
+    return CarouselSlider.builder(
+      itemCount: bannerImages.length,
+      itemBuilder: (context, index, realIndex) {
+        return Container(
+          margin: EdgeInsets.symmetric(vertical: Get.height * 0.005),
+          width: Get.width,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
+          child: Image.asset(
+            bannerImages[index],
+            fit: BoxFit.cover,
+          ),
+        );
+      },
       options: CarouselOptions(
+        aspectRatio: 2,
+        viewportFraction: 1,
+        initialPage: 0,
+        enableInfiniteScroll: true,
+        reverse: false,
         autoPlay: true,
+        autoPlayInterval: const Duration(seconds: 3),
+        autoPlayAnimationDuration: const Duration(milliseconds: 1000),
         autoPlayCurve: Curves.fastOutSlowIn,
-        autoPlayAnimationDuration: const Duration(milliseconds: 800),
-        autoPlayInterval: const Duration(seconds: 2),
         enlargeCenterPage: true,
-        aspectRatio: 2.0,
+        enlargeFactor: 0.9,
+        scrollDirection: Axis.horizontal,
       ),
     );
+    // Container(
+    //   color: Colors.red,
+    //   height: 200,
+
+    //   child: CarouselSlider(
+    //     items: bannerImages,
+    //     options: CarouselOptions(
+    //       autoPlay: true,
+    //       autoPlayCurve: Curves.fastOutSlowIn,
+    //       autoPlayAnimationDuration: const Duration(milliseconds: 800),
+    //       autoPlayInterval: const Duration(seconds: 2),
+    //       enlargeCenterPage: true,
+    //       aspectRatio: 3 / 1,
+    //     ),
+    //   ),
+    // );
   }
 }
